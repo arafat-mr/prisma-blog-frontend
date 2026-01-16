681b3b9372e6d8ea72c4f907c9b97059eca65d02
arafat-mr <arafatmr42@gmail.com>
2026-01-16 05:11:38 +0400

    implemented dashboard structure

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
-
+export const dynamic = 'force-dynamic'
 export default  async function AboutPage() {
   await new Promise((resolve)=>{
     setTimeout(resolve,2000)
   })
-  throw new Error('Error~!!')
+  // throw new Error('Error~!!')
   return (
     <div>
       ABout me
--- src/layout/Navbar.tsx
+++ src/layout/Navbar.tsx
@@ -81,6 +81,10 @@ const Navbar = ({
       title: "About",
       url: "/about",
     },
+    {
+      title: "Dashboard",
+      url: "/dashboard",
+    },
   ],
   auth = {
     login: { title: "Login", url: "/login" },
